55842695afca38aa4de555747fd7196a07dc7a5b
Jack Chen <jackchen@eduroam-int-dhcp-97-159-253.ucl.ac.uk>
2024-01-09 10:24:15 +0000

    first commit of chromaDB test

---
+++ test.py
@@ -0,0 +1,35 @@
+from datasets import load_dataset
+
+
+dataset = load_dataset("pile-of-law/pile-of-law", "sec_administrative_proceedings", split="train")
+#print(dataset[0])
+
+import chromadb
+client = chromadb.Client()
+
+
+import chromadb.utils.embedding_functions as embedding_functions
+openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+                api_key="148aa1a7a3dc4dd88004bfe29e87d2c6", # Key 1
+                api_base="https://chromadb-test.openai.azure.com/",
+                api_type="azure",
+                api_version="OpenAI",
+                model_name="text-embedding-ada-002"
+            )
+
+        
+collection = client.create_collection("pile-of-law")
+collection.add(
+    ids=[str(i) for i in range(0, 10)],  
+    documents=dataset["text"][:10],
+    uris=dataset["url"][:10] 
+)
+
+results = collection.query(
+    query_texts=["Rule 155"],
+    n_results= 5,
+    include=["documents"])
+
+print(results)
+
+#TODO: upload a.pdf and get text inside into the document instead of "a.pdf"
